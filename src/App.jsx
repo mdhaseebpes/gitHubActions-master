47c0953a236ec135d9967ec96d74cceb812d0257
Haseeb <mdhaseebpes@gmail.com>
2026-04-21 19:53:00 +0530

    fixing header

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div style={{ padding: 24, fontFamily: "system-ui" }}>
       <h1>GitHub Actions Demo (React + Vite)</h1>
-       <h3>GitHub Actions workflow test</h1>
+       <h2>GitHub Actions workflow test</h2>
       <Counter />
     </div>
   );
